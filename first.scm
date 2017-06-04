16dbc7a31859491c943a08841800b87d32ecf403
nathaniel <nathaniel@marginalia.be>
2017-06-04 11:24:38 +0200

    sunday morning

--- first.scm
+++ first.scm
@@ -23,19 +23,36 @@
                  (<CoreABNF> char-list-<CoreABNF> char-list/)
                  )
 
-(define (value?  x)    (or (string? x) (number? x) (boolean? x)
-			   (vector? x) (null? x) (pair? x) (symbol? x)))
+(define (value? x)
+  (or (string? x) (number? x) (boolean? x)
+      (vector? x) (null? x) (pair? x) (symbol? x)))
 
-(define consumed-values (consumed-objects value?))
+(define (value? x)
+  (or (symbol? x) (char? x) (pair? x)))
+
+(define consumed-values (consumed-objects val?))
+
+;; helper macro for mutually-recursive parser definitions
+
+(define-syntax vac
+  (syntax-rules ()
+    ((_ fn) (lambda args (apply fn args)))))
 
 (define consumed-values->list
   (consumed-objects-lift consumed-values))
 
 ;; shortcut for (abnf:bind (consumed-values->list ...) ... )
+
 (define-syntax bind-consumed-values->list
   (syntax-rules () 
     ((_ l p)    (bind (consumed-values->list l)  p))
-    ((_ p)      (bind (consumed-values->list)    p))
+    ((_ p)      (bind (consumed-values->list)  p))
+    ))
+
+(define-syntax bind-consumed-values->alist
+  (syntax-rules () 
+    ((_ label l p)    (bind (consumed-values->list label l)  p))
+    ((_ label p)      (bind (consumed-values->list label)  p))
     ))
 
 (define fws
@@ -53,44 +70,47 @@
    (drop-consumed (optional-sequence fws)) p 
    (drop-consumed (optional-sequence fws))))
 
-(define select-name 
-  (alternatives
-   (char-list/lit "SELECT")
-   (char-list/lit "select")))
-
-(define select-query
-  (concatenation
-   (between-fws select-name)
-   (between-fws (char-list/lit "{"))
-   
-   (between-fws (char-list/lit "}"))))
-
 (define (err s)
   (print "lexical error on stream: " s)
   `(error))
 
-
 (define PNAME_NS (between-fws (concatenation (repetition1 char-list/alpha) (char-list/lit ":"))))
 
-(define IRI_REF (between-fws (repetition1 char-list/alpha)))
+(define IRI_REF
+   (between-fws
+    (concatenation
+     (char-list/lit "<http://")
+     (repetition1 
+      (alternatives ;; should be list-from-string
+       char-list/alpha
+       (char-list/lit ".")
+       (char-list/lit "/")))
+     (char-list/lit ">"))))
 
 (define PrefixDecl
-  (concatenation
-   (between-fws (char-list/lit "PREFIX"))
-   PNAME_NS
-   IRI_REF))
+  (bind-consumed-values->alist
+   'Prefix
+   (concatenation
+    (between-fws (char-list/lit "PREFIX"))
+    PNAME_NS
+    IRI_REF)))
 
 (define Prologue (repetition PrefixDecl))
 
 (define (lit/sp str)
   (between-fws (char-list/lit str)))
 
-(define Var (lit/sp "?a"))
+(define Var
+  (bind-consumed-values->alist
+   'Var
+   (lit/sp "?a")))
 
 (define Expression (lit/sp "?b"))
 
 (define SelectClause
-  (concatenation (lit/sp "SELECT")
+  (bind-consumed-values->alist
+   'Select
+   (concatenation (lit/sp "SELECT")
 		  (optional-sequence
 		   (alternatives  (lit/sp "DISTINCT") (lit/sp "REDUCED")))
 		  (alternatives
@@ -98,23 +118,27 @@
 		    (alternatives
 		     Var
 		     (concatenation (lit/sp "(") Expression (lit/sp "AS") Var (lit/sp ")"))))
-		   (lit/sp "*"))))
+		   (lit/sp "*")))))
 
 ; ( ( Var | ( '(' Expression 'AS' Var ')' ) )+ | '*' )
 
 
 (define SelectQuery (concatenation SelectClause)) ; DatasetClause* WhereClause SolutionModifier
 
-(define Query (concatenation
-	       (bind-consumed-values->list Prologue)
-	       (bind-consumed-values->list SelectQuery)))
+(define Query
+  (bind-consumed-values->alist
+   'Query
+   (concatenation
+    (bind-consumed-values->alist 'Prologue Prologue)
+    (bind-consumed-values->alist 'SelectQuery SelectQuery))))
 ;	       (alternatives SelectQuery ))); ConstructQuery DescribeQuery AskQuery )
 ;	       ValuesClause))
 
 (require-extension lexgen)
 
-(define r (lex Prologue err "PREFIX pre: <http://www.home.com/>  
-                  PREFIX pre: <http:go/>")) ;SELECT ?a ?a"))
+(define r (lex Query err "PREFIX pre: <http://www.home.com/>  
+                  PREFIX pre: <http://www.gooogle.com/> 
+SELECT ?a ?a"))
 
 (print r)
 
